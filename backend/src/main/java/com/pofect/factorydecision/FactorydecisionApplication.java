56884a5ca32117e43bcd29097093e888c90c3b97
yyhjin <wendy0301@naver.com>
2023-11-20 16:47:28 +0900

    [chore] init

---
+++ backend/src/main/java/com/pofect/factorydecision/FactorydecisionApplication.java
@@ -0,0 +1,13 @@
+package com.pofect.factorydecision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorydecisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorydecisionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pofect/factorydecision/FactorydecisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pofect.factorydecision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorydecisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
