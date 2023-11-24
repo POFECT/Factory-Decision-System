d98a75dbdff9459584f10db64d25504381756d3a
백재원 <bjw1622@gmail.com>
2023-11-24 09:08:45 +0900

    [chore] init

--- frontend/src/@core/theme/ThemeComponent.js
+++ frontend/src/@core/theme/ThemeComponent.js
@@ -1,40 +1,44 @@
 // ** MUI Imports
-import CssBaseline from '@mui/material/CssBaseline'
-import GlobalStyles from '@mui/material/GlobalStyles'
-import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles'
+import CssBaseline from "@mui/material/CssBaseline";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import {
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from "@mui/material/styles";
 
 // ** Theme Config
-import themeConfig from 'src/configs/themeConfig'
+import themeConfig from "src/configs/themeConfig";
 
 // ** Theme Override Imports
-import overrides from './overrides'
-import typography from './typography'
+import overrides from "./overrides";
+import typography from "./typography";
 
 // ** Theme
-import themeOptions from './ThemeOptions'
+import themeOptions from "./ThemeOptions";
 
 // ** Global Styles
-import GlobalStyling from './globalStyles'
+import GlobalStyling from "./globalStyles";
 
-const ThemeComponent = props => {
+const ThemeComponent = (props) => {
   // ** Props
-  const { settings, children } = props
+  const { settings, children } = props;
 
   // ** Merged ThemeOptions of Core and User
-  const coreThemeConfig = themeOptions(settings)
+  const coreThemeConfig = themeOptions(settings);
 
   // ** Pass ThemeOptions to CreateTheme Function to create partial theme without component overrides
-  let theme = createTheme(coreThemeConfig)
+  let theme = createTheme(coreThemeConfig);
 
   // ** Continue theme creation and pass merged component overrides to CreateTheme function
   theme = createTheme(theme, {
     components: { ...overrides(theme) },
-    typography: { ...typography(theme) }
-  })
+    typography: { ...typography(theme) },
+  });
 
   // ** Set responsive font sizes to true
   if (themeConfig.responsiveFontSizes) {
-    theme = responsiveFontSizes(theme)
+    theme = responsiveFontSizes(theme);
   }
 
   return (
@@ -43,7 +47,7 @@ const ThemeComponent = props => {
       <GlobalStyles styles={() => GlobalStyling(theme)} />
       {children}
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default ThemeComponent
+export default ThemeComponent;
--- frontend/src/@core/theme/ThemeOptions.js
+++ frontend/src/@core/theme/ThemeOptions.js
@@ -1,54 +1,41 @@
 // ** MUI Theme Provider
-import { deepmerge } from '@mui/utils'
+import { deepmerge } from "@mui/utils";
 
 // ** Theme Override Imports
-import palette from './palette'
-import spacing from './spacing'
-import shadows from './shadows'
-import breakpoints from './breakpoints'
+import palette from "./palette";
+import spacing from "./spacing";
+import shadows from "./shadows";
+import breakpoints from "./breakpoints";
 
-const themeOptions = settings => {
+const themeOptions = (settings) => {
   // ** Vars
-  const { mode, themeColor } = settings
+  const { mode, themeColor } = settings;
 
   const themeConfig = {
     palette: palette(mode, themeColor),
     typography: {
-      fontFamily: [
-        'Inter',
-        'sans-serif',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"'
-      ].join(',')
+      fontFamily: ["Gowun Dodum"].join(","),
     },
     shadows: shadows(mode),
     ...spacing,
     breakpoints: breakpoints(),
     shape: {
-      borderRadius: 6
+      borderRadius: 6,
     },
     mixins: {
       toolbar: {
-        minHeight: 64
-      }
-    }
-  }
+        minHeight: 64,
+      },
+    },
+  };
 
   return deepmerge(themeConfig, {
     palette: {
       primary: {
-        ...themeConfig.palette[themeColor]
-      }
-    }
-  })
-}
+        ...themeConfig.palette[themeColor],
+      },
+    },
+  });
+};
 
-export default themeOptions
+export default themeOptions;
