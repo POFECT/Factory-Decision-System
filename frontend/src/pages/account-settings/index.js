38bc7f90cb5f684a907c189e56cf22fa52b2750c
백재원 <bjw1622@gmail.com>
2023-12-01 09:49:02 +0900

    [style] essential

--- frontend/src/pages/account-settings/index.js
+++ frontend/src/pages/account-settings/index.js
@@ -1,84 +1,89 @@
 // ** React Imports
-import { useState } from 'react'
+import { useState } from "react";
 
 // ** MUI Imports
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import TabList from '@mui/lab/TabList'
-import TabPanel from '@mui/lab/TabPanel'
-import TabContext from '@mui/lab/TabContext'
-import { styled } from '@mui/material/styles'
-import MuiTab from '@mui/material/Tab'
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import TabContext from "@mui/lab/TabContext";
+import { styled } from "@mui/material/styles";
+import MuiTab from "@mui/material/Tab";
 
 // ** Icons Imports
-import AccountOutline from 'mdi-material-ui/AccountOutline'
-import LockOpenOutline from 'mdi-material-ui/LockOpenOutline'
-import InformationOutline from 'mdi-material-ui/InformationOutline'
+import AccountOutline from "mdi-material-ui/AccountOutline";
+import LockOpenOutline from "mdi-material-ui/LockOpenOutline";
+import InformationOutline from "mdi-material-ui/InformationOutline";
 
 // ** Demo Tabs Imports
-import TabInfo from 'src/views/account-settings/TabInfo'
-import TabAccount from 'src/views/account-settings/TabAccount'
-import TabSecurity from 'src/views/account-settings/TabSecurity'
+import TabInfo from "src/views/account-settings/TabInfo";
+import TabAccount from "src/views/account-settings/TabAccount";
+import TabSecurity from "src/views/account-settings/TabSecurity";
 
 // ** Third Party Styles Imports
-import 'react-datepicker/dist/react-datepicker.css'
+import "react-datepicker/dist/react-datepicker.css";
 
 const Tab = styled(MuiTab)(({ theme }) => ({
-  [theme.breakpoints.down('md')]: {
-    minWidth: 100
+  [theme.breakpoints.down("md")]: {
+    minWidth: 100,
   },
-  [theme.breakpoints.down('sm')]: {
-    minWidth: 67
-  }
-}))
+  [theme.breakpoints.down("sm")]: {
+    minWidth: 67,
+  },
+}));
 
-const TabName = styled('span')(({ theme }) => ({
+const TabName = styled("span")(({ theme }) => ({
   lineHeight: 1.71,
-  fontSize: '0.875rem',
+  fontSize: "0.875rem",
   marginLeft: theme.spacing(2.4),
-  [theme.breakpoints.down('md')]: {
-    display: 'none'
-  }
-}))
+  [theme.breakpoints.down("md")]: {
+    display: "none",
+  },
+}));
 
 const AccountSettings = () => {
   // ** State
-  const [value, setValue] = useState('account')
+  const [value, setValue] = useState("account");
 
   const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
+    setValue(newValue);
+  };
 
   return (
     <Card>
       <TabContext value={value}>
         <TabList
           onChange={handleChange}
-          aria-label='account-settings tabs'
-          sx={{ borderBottom: theme => `1px solid ${theme.palette.divider}` }}
+          aria-label="account-settings tabs"
+          sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
         >
           <Tab
-            value='account'
+            value="account"
             label={
-              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
                 <AccountOutline />
                 <TabName>Account</TabName>
               </Box>
             }
           />
           <Tab
-            value='security'
+            value="security"
             label={
-              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Box sx={{ display: "flex", alignItems: "center" }}>
                 <LockOpenOutline />
                 <TabName>Security</TabName>
               </Box>
             }
           />
           <Tab
-            value='info'
+            value="info"
             label={
-              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Box sx={{ display: "flex", alignItems: "center" }}>
                 <InformationOutline />
                 <TabName>Info</TabName>
               </Box>
@@ -86,18 +91,18 @@ const AccountSettings = () => {
           />
         </TabList>
 
-        <TabPanel sx={{ p: 0 }} value='account'>
+        <TabPanel sx={{ p: 0 }} value="account">
           <TabAccount />
         </TabPanel>
-        <TabPanel sx={{ p: 0 }} value='security'>
+        <TabPanel sx={{ p: 0 }} value="security">
           <TabSecurity />
         </TabPanel>
-        <TabPanel sx={{ p: 0 }} value='info'>
+        <TabPanel sx={{ p: 0 }} value="info">
           <TabInfo />
         </TabPanel>
       </TabContext>
     </Card>
-  )
-}
+  );
+};
 
-export default AccountSettings
+export default AccountSettings;
