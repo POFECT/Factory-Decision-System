f84ee5d2d36d07fb8b9d5e2b10cc30f40c0a90d7
yyhjin <wendy0301@naver.com>
2023-12-04 11:04:27 +0900

    [feat] axios 모듈화

---
+++ frontend/src/api/MainCapacityApi.js
@@ -0,0 +1,29 @@
+import { axiosApi } from "./api";
+
+const MainCapacityApi = {
+  getList: async (callback) => {
+    await axiosApi()
+      .get("/main")
+      .then((response) => {
+        callback && callback(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {});
+  },
+
+  getOrder: async (no, callback) => {
+    await axiosApi()
+      .get(`/main/${no}`)
+      .then((response) => {
+        callback && callback(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {});
+  },
+};
+
+export default MainCapacityApi;
