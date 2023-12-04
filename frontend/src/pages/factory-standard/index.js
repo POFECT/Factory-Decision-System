c298fd63075fcd30a79efa1cde5e23ff929c913c
shin-6-0 <syk6715@naver.com>
2023-12-04 20:15:30 +0900

    [style]확통 Grid 생성

--- frontend/src/pages/factory-standard/index.js
+++ frontend/src/pages/factory-standard/index.js
@@ -5,34 +5,34 @@ import "react-datasheet-grid/dist/style.css";
 import { Grid, Typography, Button, Select, MenuItem, FormControl, InputLabel, OutlinedInput, } from "@mui/material";
 import FactoryStandardApi from "src/api/FactoryStandardApi";
 
-function MyCell(props) {
-  let style = {
-    minWidth: props.width,
-    maxWidth: props.width,
-    minHeight: props.height,
-    maxHeight: props.height === "auto" ? "none" : props.height,
-    ...props.style,
-  };
-  const apiRef = useGridApiContext();
-  const row = apiRef.current.getRow(props.rowId);
-  if (row && row.rowSpan && row.rowSpan[props.column.field]) {
-    const span = row.rowSpan[props.column.field];
-    style = {
-      ...style,
-      minHeight: props.height * span,
-      maxHeight: props.height * span,
-      backgroundColor: "gray",
-      zIndex: 1,
-    };
-  }
-  return <GridCell {...props} style={style} />;
-}
+// function MyCell(props) {
+//   let style = {
+//     minWidth: props.width,
+//     maxWidth: props.width,
+//     minHeight: props.height,
+//     maxHeight: props.height === "auto" ? "none" : props.height,
+//     ...props.style,
+//   };
+//   const apiRef = useGridApiContext();
+//   const row = apiRef.current.getRow(props.rowId);
+//   if (row && row.rowSpan && row.rowSpan[props.column.field]) {
+//     const span = row.rowSpan[props.column.field];
+//     style = {
+//       ...style,
+//       minHeight: props.height * span,
+//       maxHeight: props.height * span,
+//       backgroundColor: "gray",
+//       zIndex: 1,
+//     };
+//   }
+//   return <GridCell {...props} style={style} />;
+// }
 const Capacity = () => {
   /* Data */
   const [possibleList,setPossibleList]=useState([]);//가통리스트
   const [confirmList,setConfirmList]=useState([]);//확통리스트
   const [millCd,setMillCd]=useState([]);//소구분
-
+  
   useEffect(()=>{
     FactoryStandardApi.getGridList((data)=>{
       const dataMap=data.reduce((acc,[code,processCd,feasibleRoutingGroup])=>{
@@ -51,17 +51,31 @@ const Capacity = () => {
     });
   },[]);
 
-  const columns = [
-    { field: "code",headerName:"Code", width:100, type:'number'},
-    { field: "10", headerName: "제강", width: 100, type:'number' },
-    { field: "20", headerName: "열연", width: 100, editable: true,     type:'number'  },
-    { field: "30", headerName: "열연정정", width: 100, type:'number'  },
-    { field: "40", headerName: "냉간압연", width: 100, type:'number'  },
-    { field: "50", headerName: "1차소둔", width: 100, type:'number'  },
-    { field: "60", headerName: "2차소둔", width: 100, type:'number'  },
-    { field: "70", headerName: "도금", width: 100, type:'number'  },
-    { field: "80", headerName: "정정", width: 100, type:'number'  },
+  //가통 컬럼
+  const possibleColumns = [
+    { field: "code",headerName:"Code", width:120, type:'number',alignItems:'left'},
+    { field: "10", headerName: "제강", width:120, type:'number' },
+    { field: "20", headerName: "열연", width:120, editable: true,     type:'number'  },
+    { field: "30", headerName: "열연정정", width:120, type:'number'  },
+    { field: "40", headerName: "냉간압연", width:120, type:'number'  },
+    { field: "50", headerName: "1차소둔", width:120, type:'number'  },
+    { field: "60", headerName: "2차소둔", width:120, type:'number'  },
+    { field: "70", headerName: "도금", width:120, type:'number'  },
+    { field: "80", headerName: "정정", width:120, type:'number'  },
   ];
+
+  //확통 컬럼
+  const confirmColumns=[
+    { field: "code",headerName:"Code", width:120, type:'number'},
+    { field: "10", headerName: "제강", width:120, type:'number' },
+    { field: "20", headerName: "열연", width:120, editable: true,     type:'number'  },
+    { field: "30", headerName: "열연정정", width:120, type:'number'  },
+    { field: "40", headerName: "냉간압연", width:120, type:'number'  },
+    { field: "50", headerName: "1차소둔", width:120, type:'number'  },
+    { field: "60", headerName: "2차소둔", width:120, type:'number'  },
+    { field: "70", headerName: "도금", width:120, type:'number'  },
+    { field: "80", headerName: "정정", width:120, type:'number'  },
+  ]
 
   return (
     <div style={{ height: "500px", width: "100%" }}>
@@ -116,13 +130,26 @@ const Capacity = () => {
           </Button>
         </div>
       </div>
-      <div style={{height:400}}>
+      <div style={{height:400, marginBottom:20}}>
       <DataGrid
         //disableRowSelectionOnClick
         rows={possibleList}
-        columns={columns}
+        columns={possibleColumns}
         onCellClick={(e) => {
+          
           setPossibleList(e);
+          console.log('소구분 : '+e);
+        }}
+        hideFooter = {true}
+      /></div>
+      <div style={{height:300}}>
+      <DataGrid
+        //disableRowSelectionOnClick
+        rows={confirmList}
+        columns={confirmColumns}
+        onCellClick={(e) => {
+          
+          setConfirmList(e);
           console.log('소구분 : '+e);
         }}
         hideFooter = {true}
