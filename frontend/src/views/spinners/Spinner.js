a4b319db43b9485116d499cad27184faf807dbed
백재원 <bjw1622@gmail.com>
2023-12-12 16:09:44 +0900

    [feat]  랜더링 시 스피너추가

---
+++ frontend/src/views/spinners/Spinner.js
@@ -0,0 +1,20 @@
+// Spinner.js
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const Spinner = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <CircularProgress />
+    </div>
+  );
+};
+
+export default Spinner;
