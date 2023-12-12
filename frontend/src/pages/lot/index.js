4b4042c92468d5a5a98e9cfc6213b03a366e0f54
bongdaring <chlwlsdud258@gmail.com>
2023-12-12 20:27:23 +0900

    [feat] 출강Lot 화면 연동

--- frontend/src/pages/lot/index.js
+++ frontend/src/pages/lot/index.js
@@ -59,54 +59,55 @@ const Lot = () => {
     useEffect(() => {
         LotApi.getList((data) => {
             const resData = data.response;
+            const testNum = 2;
 
             const resultData = resData.map((item, index) => {
                 const newItem = { ...item, id: index + 1 };
-                if (item && item.faConfirmFlag === "E"){
-                    newItem = {...newItem, faConfirmFlag : "투입대기", rowSpan: {smSteelGrdN:2}}
-                    if (item && item.widthGroups && item.widthGroups.width_970_stand){
-                        newItem = {...newItem, width_970_stand:item.widthGroups.width_970_stand}
+                if (item && item.faConfirmFlag === "E") {
+                    newItem = { ...newItem, faConfirmFlag: "투입대기", rowSpan: { smSteelGrdN: testNum } }
+                    if (item && item.widthGroups && item.widthGroups.width_970_stand) {
+                        newItem = { ...newItem, width_970_stand: item.widthGroups.width_970_stand }
                     }
-                    if (item && item.widthGroups && item.widthGroups.width_1270_stand){
-                        newItem = {...newItem, width_1270_stand:item.widthGroups.width_1270_stand}
+                    if (item && item.widthGroups && item.widthGroups.width_1270_stand) {
+                        newItem = { ...newItem, width_1270_stand: item.widthGroups.width_1270_stand }
                     }
-                    if (item && item.widthGroups && item.widthGroups.width_1570_stand){
-                        newItem = {...newItem, width_1570_stand:item.widthGroups.width_1570_stand}
+                    if (item && item.widthGroups && item.widthGroups.width_1570_stand) {
+                        newItem = { ...newItem, width_1570_stand: item.widthGroups.width_1570_stand }
                     }
-                    if (item && item.widthGroups && item.widthGroups.width_over_15702_stand){
-                        newItem = {...newItem, width_over_15702_stand:item.widthGroups.width_over_15702_stand}
+                    if (item && item.widthGroups && item.widthGroups.width_over_15702_stand) {
+                        newItem = { ...newItem, width_over_15702_stand: item.widthGroups.width_over_15702_stand }
                     }
                     return newItem;
                 } else if (item && item.faConfirmFlag === "F") {
-                    newItem = {...newItem, faConfirmFlag : "기투입"}
-                    if (item && item.widthGroups && item.widthGroups.width_9701){
-                        newItem = {...newItem, width_9701:item.widthGroups.width_9701}
+                    newItem = { ...newItem, faConfirmFlag: "기투입" }
+                    if (item && item.widthGroups && item.widthGroups.width_9701) {
+                        newItem = { ...newItem, width_9701: item.widthGroups.width_9701 }
                     }
-                    if (item && item.widthGroups && item.widthGroups.width_9702){
-                        newItem = {...newItem, width_9702:item.widthGroups.width_9702}
+                    if (item && item.widthGroups && item.widthGroups.width_9702) {
+                        newItem = { ...newItem, width_9702: item.widthGroups.width_9702 }
                     }
-                    if (item && item.widthGroups && item.widthGroups.width_12701){
-                        newItem = {...newItem, width_12701:item.widthGroups.width_12701}
+                    if (item && item.widthGroups && item.widthGroups.width_12701) {
+                        newItem = { ...newItem, width_12701: item.widthGroups.width_12701 }
                     }
-                    if (item && item.widthGroups && item.widthGroups.width_12702){
-                        newItem = {...newItem, width_12702:item.widthGroups.width_12702}
+                    if (item && item.widthGroups && item.widthGroups.width_12702) {
+                        newItem = { ...newItem, width_12702: item.widthGroups.width_12702 }
                     }
-                    if (item && item.widthGroups && item.widthGroups.width_15701){
-                        newItem = {...newItem, width_15701:item.widthGroups.width_15701}
+                    if (item && item.widthGroups && item.widthGroups.width_15701) {
+                        newItem = { ...newItem, width_15701: item.widthGroups.width_15701 }
                     }
-                    if (item && item.widthGroups && item.widthGroups.width_15702){
-                        newItem = {...newItem, width_15702:item.widthGroups.width_15702}
+                    if (item && item.widthGroups && item.widthGroups.width_15702) {
+                        newItem = { ...newItem, width_15702: item.widthGroups.width_15702 }
                     }
-                    if (item && item.widthGroups && item.widthGroups.width_over_15701){
-                        newItem = {...newItem, width_over_15701:item.widthGroups.width_over_15701}
+                    if (item && item.widthGroups && item.widthGroups.width_over_15701) {
+                        newItem = { ...newItem, width_over_15701: item.widthGroups.width_over_15701 }
                     }
-                    if (item && item.widthGroups && item.widthGroups.width_over_15702){
-                        newItem = {...newItem, width_over_15702:item.widthGroups.width_over_15702}
+                    if (item && item.widthGroups && item.widthGroups.width_over_15702) {
+                        newItem = { ...newItem, width_over_15702: item.widthGroups.width_over_15702 }
                     }
                 }
-                return { ...newItem} 
+                return { ...newItem }
             });
-            
+
             setLotData(resultData);
         });
     }, []);
@@ -220,9 +221,52 @@ const Lot = () => {
         { field: "width_over_15701", headerName: "1", width: 80, sortable: false, headerAlign: "center" },
         { field: "width_over_15702", headerName: "2", width: 80, sortable: false, headerAlign: "center" },
         { field: "width_over_15702_stand", headerName: "대기", width: 80, sortable: false, headerAlign: "center" },
-        { field: "합계", headerName: "1", width: 80, sortable: false, headerAlign: "center" },
-        { field: "합계2", headerName: "2", width: 80, sortable: false, headerAlign: "center" },
-        { field: "합계대기", headerName: "대기", width: 80, sortable: false, headerAlign: "center" },
+
+        // { field: "합계", headerName: "1", width: 80, sortable: false, headerAlign: "center" },
+        {
+            field: "합계",
+            headerName: "1",
+            width: 80,
+            valueGetter: (params) => {
+                const width_9701 = params.row.width_9701 || 0;
+                const width_12701 = params.row.width_12701 || 0;
+                const width_15701 = params.row.width_15701 || 0;
+                const width_over_15701 = params.row.width_over_15701 || 0;
+
+                return width_9701 + width_12701 + width_15701 + width_over_15701 || "";
+            },
+            headerAlign: "center"
+        },
+        {
+            field: "합계2",
+            headerName: "2",
+            width: 80,
+            valueGetter: (params) => {
+                const width_9702 = params.row.width_9702 || 0;
+                const width_12702 = params.row.width_12702 || 0;
+                const width_15702 = params.row.width_15702 || 0;
+                const width_over_15702 = params.row.width_over_15702 || 0;
+
+                return width_9702 + width_12702 + width_15702 + width_over_15702 || "";
+            },
+            headerAlign: "center"
+        },
+        // { field: "합계2", headerName: "2", width: 80, sortable: false, headerAlign: "center" },
+        {
+            field: "합계대기",
+            headerName: "대기",
+            width: 80,
+            valueGetter: (params) => {
+                const width_970_stand = params.row.width_970_stand || 0;
+                const width_1270_stand = params.row.width_1270_stand || 0;
+                const width_1570_stand = params.row.width_1570_stand || 0;
+                const width_over_15702_stand = params.row.width_over_15702_stand || 0;
+
+                return width_970_stand + width_1270_stand + width_1570_stand + width_over_15702_stand || "";
+            },
+            headerAlign: "center"
+        },
+        // { field: "합계대기", headerName: "대기", width: 80, sortable: false, headerAlign: "center" },
     ];
 
     const columnGroupingModel = [
@@ -252,7 +296,7 @@ const Lot = () => {
             headerAlign: "center"
         },
     ];
-    
+
 
     const customStyles = {
         control: (provided, state) => ({
@@ -263,6 +307,24 @@ const Lot = () => {
 
     const handleCheckboxChange = () => {
         setIsChecked(!isChecked);
+    };
+
+    const [totalSum, setTotalSum] = useState(0);
+
+    useEffect(() => {
+        calculateSum();
+    }, []); // Runs once on initial render
+
+    const calculateSum = () => {
+        const sum = lotData.reduce((accumulator, row) => {
+            const width_9702 = row.width_9702 || 0;
+            const width_12702 = row.width_12702 || 0;
+            const width_15702 = row.width_15702 || 0;
+            const width_over_15702 = row.width_over_15702 || 0;
+            return accumulator + (width_9702 + width_12702 + width_15702 + width_over_15702);
+        }, "");
+
+        setTotalSum(sum);
     };
 
     return (
@@ -331,6 +393,7 @@ const Lot = () => {
                     <input
                         type="checkbox"
                         checked={isChecked}
+                        defaultChecked={true}
                         onChange={handleCheckboxChange}
                     />
                     <label style={{ fontSize: "16px", paddingLeft: 5 }}>
