08bb36bef57eb6d237b8b77f18876f00dff3dd86
s2lilac <s2cherryy@gmail.com>
2023-12-13 16:44:28 +0900

    [feat] 능력 차트 ui 수정 및 legend 추가

--- frontend/src/pages/capacity/d3-heat.js
+++ frontend/src/pages/capacity/d3-heat.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from 'react';
 import * as d3 from 'd3';
 import { useSpring, animated } from 'react-spring';
 
-const MyD3Heatmap = ({ capacity , marginLeft}) => {
+const MyD3Heatmap = ({ capacity }) => {
   // const xLabels = capacity ? capacity.map(item => item.firmPsFacTp) : [];
   // const yLabels = capacity ? capacity.map(item => item.processCd) : [];
   const data = capacity ? capacity.map(item => [Number(item.processCd) / 10, Number(item.firmPsFacTp), item.remainQty]) : [];
   const reshapedData = data.map(([x, y, value]) => [x, y, value]);
 
 
-   const xLabels = ['1공장', '2공장', '3공장'];
-  const yLabels = ['제강', '열연', '열연정정', '냉간압연', '1차소둔', '2차소둔', '도금', '정정', '빈값'];
+  const xLabels = ['1공장', '2공장', '3공장'];
+  const yLabels = ['제강', '열연', '열연정정', '냉간압연', '1차소둔', '2차소둔', '도금', '정정', ];
 
   const xMax = Math.max(...reshapedData.map(([x]) => x));
   const yMax = Math.max(...reshapedData.map(([, y]) => y));
@@ -22,10 +22,21 @@ const MyD3Heatmap = ({ capacity , marginLeft}) => {
   });
 
   const cleanedResultArray = resultArray.map(arr => arr.slice(0, -1));
-console.log("))))))",cleanedResultArray)
+    console.log("))))))",cleanedResultArray)
   const transformedData = cleanedResultArray;
-  // const xLabels = ['1공장', '2공장', '3공장'];
-  // const yLabels = ['제강', '열연', '열연정정', '냉간압연', '1차소둔', '2차소둔', '도금', '정정', '빈값'];
+
+  const cellData = [
+    ["1제강", "2제강", ""],
+    ["1열연", "2열연", ""],
+    ["1열연정정", "2열연정정", ""],
+    ["1PCM", "2PCM", "3PCM"],
+    ["1CAL","2CAL","3CAL"],
+    ["1ACL","", "3ACL"],
+    ["", "2EGL", "3EGL"],
+    ["1RCL", "", ""]
+  ];
+
+  const legendData = ["여유", "보통", "부하"]; 
 
   const [hoveredCell, setHoveredCell] = useState(null);
   const [clickedCell, setClickedCell] = useState(null);
@@ -40,13 +51,19 @@ console.log("))))))",cleanedResultArray)
     const xSize = 100;
     const ySize = 70; 
     const textColor = "#000";
+    const legendWidth = 90;
+    const legendHeight = 30;
 
     const colorScale = d3.scaleSequential()
       .domain([0, d3.max(transformedData.flat())])
       .interpolator(d3.interpolateRgb('#e61919', '#F8E0E0'));
 
-const svg = d3.select('#heatmap-svg')
-  .attr('transform', `translate(${marginLeft}, 0)`); 
+    const legendColorScale = d3.scaleSequential()
+    .domain([0, legendData.length - 1])
+    .interpolator(d3.interpolateRgb('#F8E0E0','#e61919'));
+
+    const svg = d3.select('#heatmap-svg')
+      .attr('transform', 'translate(-30, 0)');
 
     svg.selectAll('*').remove(); 
 
@@ -60,33 +77,52 @@ const svg = d3.select('#heatmap-svg')
       .attr('transform', `translate(${xSize / 2}, -6)`);
 
     svg.selectAll('.yLabel')
-      .data(yLabels.flat())
-      // .enter().append('text')
-      .text(d => d !== 0 ? d : '')
-      .attr('x', 0)
-      .attr('y', (d, i) => i * ySize)
-      // .style('text-anchor', 'end')
-      // .style('text-anchor', 'start')
+      .data(yLabels)
+      .enter().append('text')
+      .text(d => d !== 0 ? d : '').raise()
+      .attr('x', 110)
+      .attr('y', (d, i) => i * ySize-6.5 )  
+      .style('text-anchor', 'end')
+      // .style('text-anchor', '')
       // .style('dominant-baseline', 'middle')
+        .style('pointer-events', 'none')
+        .style('font-size', '13px')
+
       .style('fill', textColor)
       .attr('transform', `translate(-6, ${ySize / 1.5})`);
- svg.selectAll('.cellText')
-      .data(transformedData.flat())
+
+    const legend = svg.selectAll('.legend')
+    .data(legendData)
+    .enter().append('rect')
+    .attr('class', 'legend')
+    .attr('width', legendWidth)
+    .attr('height', 15)
+    .attr('x', (d, i) => i * legendWidth+150)
+    .attr('y', yLabels.length * ySize + 30) 
+  .style('fill', (d, i) => legendColorScale(i));
+
+    svg.selectAll('.legendText')
+      .data(legendData)
       .enter().append('text')
-      .text(d => d !== 0 ? d : '')
-      .attr('x', (d, i) => (i % xLabels.length) * xSize + xSize / 2)
-      .attr('y', (d, i) => Math.floor(i / xLabels.length) * ySize + ySize / 2)
-      .style('text-anchor', 'middle')
+      .attr('class', 'legendText')
+      .text(d => d)
+      .attr('x', (d, i) =>( i * legendWidth + legendWidth / 2) +170)
+      .attr('y', yLabels.length * ySize + 30 + 25)
+      .style('font-size', '14px')
+ 
       .style('dominant-baseline', 'middle')
       .style('fill', textColor);
-      
+          
+
+
     const cells = svg.selectAll('.cell')
+      .lower()
       .data(transformedData.flat())
       .enter().append('rect')
       .attr('class', 'cell')
       .attr('width', xSize)
       .attr('height', ySize)
-      .attr('x', (d, i) => (i % xLabels.length) * xSize)
+      .attr('x', (d, i) => (i % xLabels.length) * xSize+120)
       .attr('y', (d, i) => Math.floor(i / xLabels.length) * ySize)
       .attr('rx', 4)
       .attr('ry', 4)
@@ -103,24 +139,27 @@ const svg = d3.select('#heatmap-svg')
         setClickedCell(d);
       });
 
+
     svg.selectAll('.cellText')
-      .data(transformedData.flat())
+      .data(cellData.flat())
       .enter().append('text')
       .text(d => d !== 0 ? d : '')
-      .attr('x', (d, i) => (i % xLabels.length) * xSize + xSize / 2)
+      .attr('x', (d, i) => (i % xLabels.length) * xSize + xSize / 2 +120)
       .attr('y', (d, i) => Math.floor(i / xLabels.length) * ySize + ySize / 2)
       .style('text-anchor', 'middle')
       .style('dominant-baseline', 'middle')
+        .style('font-size', '14px')
+
       .style('fill', textColor);
-  }, [transformedData, xLabels, yLabels, hoveredCell]);
+  }, [transformedData,cellData, xLabels, yLabels, hoveredCell]);
 
   return (
     <animated.div style={fadeInAnimation}>
       <div>
         {/* <p>Hovered Cell: {hoveredCell}</p> */}
-        <p>Clicked Cell: {clickedCell}</p>
+        <p style={{ marginLeft: 5, marginBottom: 25 }}>선택된 공장의 잔여량: {clickedCell}</p>
       </div>
-      <svg id="heatmap-svg" width={xLabels.length * 100} height={yLabels.length * 80}>
+      <svg id="heatmap-svg" width={1200} height={yLabels.length * 80}>
       </svg>
     </animated.div>
   );
