9c0d05aaa4b11aee058c08295018d76adbd9d503
shin-6-0 <syk6715@naver.com>
2023-12-14 19:04:15 +0900

    [feat]login시 가능/확통기준으로 이동

--- frontend/src/pages/login&userInfo/login/index.js
+++ frontend/src/pages/login&userInfo/login/index.js
@@ -235,7 +235,7 @@ const LoginPage = () => {
               size="large"
               variant="contained"
               sx={{ marginBottom: 7, width: "100%" }}
-              onClick={() => router.push("/")}
+              onClick={() => router.push("/main-capacity/")}
             >
               Login
             </Button>
