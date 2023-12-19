a0ad669a9bd4039b71e3b1e94387fb758a385425
bongdaring <chlwlsdud258@gmail.com>
2023-12-19 15:16:01 +0900

    [feat] 사이즈 기준 예상 설계 모달 구현

--- frontend/src/pages/size-standard/size-design-modal.js
+++ frontend/src/pages/size-standard/size-design-modal.js
@@ -66,41 +66,90 @@ const SizeDesignModal = ({ open, handleClose }) => {
         { id: 1, name: "두께", value: "thick", inputValue: 0 },
         { id: 2, name: "폭", value: "width", inputValue: 0 },
         { id: 3, name: "길이", value: "length", inputValue: 0 },
-        { id: 4, name: "단중", value: "roll",  inputValue: 0 }
+        { id: 4, name: "단중", value: "roll", inputValue: 0 }
     ]);
 
     const [resultSize, setResultSize] = useState([]);
+    const [resultFlag, setResultFlag] = useState(false);
+
+    const resultColumns = [
+        { field: "processCD", headerName: "공정", sortable: false, headerAlign: 'center' },
+        { field: "value", headerName: "가능한 공장", sortable: false, width: 150, headerAlign: 'center', type: 'number', },
+    ];
 
     const testClick = () => {
+
         const requestSize = {
             thick: rows[0].inputValue,
             width: rows[1].inputValue,
             length: rows[2].inputValue,
             roll: rows[3].inputValue,
         }
-        SizeStandardApi.getSizeDesign(requestSize, (data) => {
-            setResultSize(data.response);
-        })
-        console.log(resultSize);
 
+        SizeStandardApi.getSizeDesign(requestSize, (data) => {
+            const resData = data.response;
+
+            const resultData = resData.map((item, index) => {
+                const newItem = { ...item, id: index + 1 };
+                if (item.processCD === "10") {
+                    const value = item.firmPsFacTpList.join(', ');
+                    return { ...newItem, processCD: "제강", value: value }
+                } else if (item.processCD === "20") {
+                    const value = item.firmPsFacTpList.join(', ');
+                    return { ...newItem, processCD: "열연", value: value }
+                } else if (item.processCD === "30") {
+                    const value = item.firmPsFacTpList.join(', ');
+                    return { ...newItem, processCD: "열연정정", value: value }
+                } else if (item.processCD === "40") {
+                    const value = item.firmPsFacTpList.join(', ');
+                    return { ...newItem, processCD: "냉간압연", value: value }
+                } else if (item.processCD === "50") {
+                    const value = item.firmPsFacTpList.join(', ');
+                    return { ...newItem, processCD: "1차소둔", value: value }
+                } else if (item.processCD === "60") {
+                    const value = item.firmPsFacTpList.join(', ');
+                    return { ...newItem, processCD: "2차소둔", value: value }
+                } else if (item.processCD === "70") {
+                    const value = item.firmPsFacTpList.join(', ');
+                    return { ...newItem, processCD: "도금", value: value }
+                } else if (item.processCD === "80") {
+                    const value = item.firmPsFacTpList.join(', ');
+                    return { ...newItem, processCD: "정정", value: value }
+                }
+
+                return newItem;
+            })
+
+
+            setResultSize(resultData);
+        })
+
+        setResultFlag(true);
     }
 
     const handleCellEditCommit = (params) => {
         const updatedList = rows.map((item) =>
-          item.id === params.id ? params : item
+            item.id === params.id ? params : item
         );
         setRows(updatedList);
-      };
+    };
+
+    const clearResult = () => {
+        setResultSize([]);
+        setResultFlag(false);
+    }
 
     return (
-        <Dialog open={open} onClose={handleClose} sx={{ width: '100%' }} maxWidth="xl">
+        <Dialog open={open} onClose={handleClose} sx={{ width: '100%'}} maxWidth="xl">
             <DialogTitle>
-                <Grid item xs={12} sx={{ paddingBottom: 4 }}>
+                <Grid item xs={12}>
                     <Typography variant="h5">사이즈 기준 임시 설계</Typography>
                 </Grid>
             </DialogTitle>
             <DialogContent>
-                <DialogContentText>
+                <DialogContentText style={{
+                    height: "400px"
+                }}>
                     <div style={{
                         display: "flex",
                         flexDirection: "row",
@@ -112,118 +161,150 @@ const SizeDesignModal = ({ open, handleClose }) => {
                                 size="small"
                                 type="submit"
                                 variant="contained"
+                                onClick={clearResult}
+                                style={{ backgroundColor: "#BE2E22", width: "80%", marginRight: "15px" }}
+                            >
+                                초기화
+                            </Button>
+                        </div>
+                        <div>
+                            <Button
+                                size="small"
+                                type="submit"
+                                variant="contained"
                                 onClick={testClick}
-                                style={{ backgroundColor: "#E29E21", width: "50%", marginRight: "15px" }}
+                                style={{ backgroundColor: "#E29E21", width: "50%", marginRight: "15px",marginLeft:"0px" }}
                             >
                                 설계
                             </Button>
                         </div>
                     </div>
-                    <div style={{ height: 300, display: "flex", justifyContent: "center", }}>
-                        <Card
-                            elevation={3}
-                            style={{
-                                flexBasis: "85",
-                                marginRight: "16px ",
-                                padding: "10px",
-                                height: "100%",
-                            }}
-                        >
-                            <Box
-                                sx={{
+                    <div style={{ height: 300, display: "flex", justifyContent: "space-between", flexDirection: "row", }}>
+                        <div>
+                            <Card
+                                elevation={3}
+                                style={{
+                                    flexBasis: "85",
+                                    marginRight: "16px ",
+                                    padding: "10px",
                                     height: "100%",
-                                    width: "100%",
-
-                                    "& .custom-data-grid .MuiDataGrid-columnsContainer, & .custom-data-grid .MuiDataGrid-cell":
-                                    {
-                                        borderBottom: "1px solid rgba(225, 234, 239, 1)",
-                                        borderRight: "1px solid rgba(225, 234, 239, 1)",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
-                                    },
-                                    "& .custom-data-grid .MuiDataGrid-columnHeader": {
-                                        cursor: "pointer",
-                                        borderBottom: "1px solid rgba(225, 234, 239, 1)",
-                                        borderRight: "1px solid rgba(225, 234, 239, 1)",
-                                    },
-                                    "& .custom-data-grid .MuiDataGrid-columnHeader--filledGroup  .MuiDataGrid-columnHeaderTitleContainer":
-                                    {
-                                        borderBottomStyle: "none",
-                                    },
-
-                                    "& .custom-data-grid .MuiDataGrid-root": {
-                                        paddingBottom: "0px",
-                                    }, "& .custom-data-grid .MuiDataGrid-columnHeadersInner": {
-                                        backgroundColor: "#F5F9FF",
-                                    },
                                 }}
                             >
-                                <DataGrid
-                                    className="custom-data-grid"
-                                    columns={columns}
-                                    rows={rows}
-                                    disableRowSelectionOnClick
-                                    rowHeight={31}
-                                    disableColumnFilter
-                                    disableColumnMenu
-                                    hideFooterPagination={true}
-                                    hideFooter={true}
-                                    processRowUpdate={(newVal) => {
-                                        handleCellEditCommit(newVal)
-                                        return newVal;
-                                      }}
-                                />
-                            </Box>
-                        </Card>
+                                <Box
+                                    sx={{
+                                        height: "100%",
+                                        width: "100%",
+
+                                        "& .custom-data-grid .MuiDataGrid-columnsContainer, & .custom-data-grid .MuiDataGrid-cell":
+                                        {
+                                            borderBottom: "1px solid rgba(225, 234, 239, 1)",
+                                            borderRight: "1px solid rgba(225, 234, 239, 1)",
+                                            display: "flex",
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                        },
+                                        "& .custom-data-grid .MuiDataGrid-columnHeader": {
+                                            cursor: "pointer",
+                                            borderBottom: "1px solid rgba(225, 234, 239, 1)",
+                                            borderRight: "1px solid rgba(225, 234, 239, 1)",
+                                        },
+                                        "& .custom-data-grid .MuiDataGrid-columnHeader--filledGroup  .MuiDataGrid-columnHeaderTitleContainer":
+                                        {
+                                            borderBottomStyle: "none",
+                                        },
+
+                                        "& .custom-data-grid .MuiDataGrid-root": {
+                                            paddingBottom: "0px",
+                                        }, "& .custom-data-grid .MuiDataGrid-columnHeadersInner": {
+                                            backgroundColor: "#F5F9FF",
+                                        },
+                                    }}
+                                >
+                                    <DataGrid
+                                        className="custom-data-grid"
+                                        columns={columns}
+                                        rows={rows}
+                                        disableRowSelectionOnClick
+                                        rowHeight={31}
+                                        disableColumnFilter
+                                        disableColumnMenu
+                                        hideFooterPagination={true}
+                                        hideFooter={true}
+                                        processRowUpdate={(newVal) => {
+                                            handleCellEditCommit(newVal)
+                                            return newVal;
+                                        }}
+                                    />
+                                </Box>
+                            </Card>
+                        </div>
+                        <div>
+                            {resultFlag ? <div style={{ height: 300, display: "flex", justifyContent: "center", }}>
+                                <Card
+                                    elevation={3}
+                                    style={{
+                                        flexBasis: "85",
+                                        marginRight: "16px ",
+                                        padding: "10px",
+                                        height: "110%",
+                                    }}
+                                >
+                                    <Box
+                                        sx={{
+                                            height: "100%",
+                                            width: "100%",
+
+                                            "& .custom-data-grid .MuiDataGrid-columnsContainer, & .custom-data-grid .MuiDataGrid-cell":
+                                            {
+                                                borderBottom: "1px solid rgba(225, 234, 239, 1)",
+                                                borderRight: "1px solid rgba(225, 234, 239, 1)",
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "center",
+                                            },
+                                            "& .custom-data-grid .MuiDataGrid-columnHeader": {
+                                                cursor: "pointer",
+                                                borderBottom: "1px solid rgba(225, 234, 239, 1)",
+                                                borderRight: "1px solid rgba(225, 234, 239, 1)",
+                                            },
+                                            "& .custom-data-grid .MuiDataGrid-columnHeader--filledGroup  .MuiDataGrid-columnHeaderTitleContainer":
+                                            {
+                                                borderBottomStyle: "none",
+                                            },
+
+                                            "& .custom-data-grid .MuiDataGrid-root": {
+                                                paddingBottom: "0px",
+                                            }, "& .custom-data-grid .MuiDataGrid-columnHeadersInner": {
+                                                backgroundColor: "#F5F9FF",
+                                            },
+                                        }}
+
+                                    >
+                                        <DataGrid
+                                            className="custom-data-grid"
+                                            columns={resultColumns}
+                                            rows={resultSize}
+                                            disableRowSelectionOnClick
+                                            rowHeight={31}
+                                            disableColumnFilter
+                                            disableColumnMenu
+                                            hideFooterPagination={true}
+                                            hideFooter={true}
+                                            processRowUpdate={(newVal) => {
+                                                handleCellEditCommit(newVal)
+                                                return newVal;
+                                            }}
+                                        />
+
+                                    </Box>
+                                </Card>
+                            </div> : ""}
+                        </div>
+
                     </div>
 
-                    {resultSize.size == undefined ? "" : <div style={{ height: 300, display: "flex", justifyContent: "center", }}>
-                        <Card
-                            elevation={3}
-                            style={{
-                                flexBasis: "85",
-                                marginRight: "16px ",
-                                padding: "10px",
-                                height: "100%",
 
-                            }}
-                        >
-                            <Box
-                                sx={{
-                                    height: "100%",
-                                    width: "100%",
 
-                                    "& .custom-data-grid .MuiDataGrid-columnsContainer, & .custom-data-grid .MuiDataGrid-cell":
-                                    {
-                                        borderBottom: "1px solid rgba(225, 234, 239, 1)",
-                                        borderRight: "1px solid rgba(225, 234, 239, 1)",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
-                                    },
-                                    "& .custom-data-grid .MuiDataGrid-columnHeader": {
-                                        cursor: "pointer",
-                                        borderBottom: "1px solid rgba(225, 234, 239, 1)",
-                                        borderRight: "1px solid rgba(225, 234, 239, 1)",
-                                    },
-                                    "& .custom-data-grid .MuiDataGrid-columnHeader--filledGroup  .MuiDataGrid-columnHeaderTitleContainer":
-                                    {
-                                        borderBottomStyle: "none",
-                                    },
-
-                                    "& .custom-data-grid .MuiDataGrid-root": {
-                                        paddingBottom: "0px",
-                                    }, "& .custom-data-grid .MuiDataGrid-columnHeadersInner": {
-                                        backgroundColor: "#F5F9FF",
-                                    },
-                                }}
-                            >
-                                
-                            </Box>
-                        </Card>
-                    </div>}
-                    
                 </DialogContentText>
 
             </DialogContent>
