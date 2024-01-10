23d62c8f1ae479f1304be7787a769e7cff42714b
yyhjin <wendy0301@naver.com>
2024-01-10 15:12:00 +0900

    [style] stepper style

--- frontend/src/pages/log/index.js
+++ frontend/src/pages/log/index.js
@@ -310,16 +310,6 @@ const Log = () => {
                 </Step>
               ))}
             </Stepper>
-            {activeStep === steps.length && (
-              <Paper square elevation={0} sx={{ p: 3 }}>
-                <Typography>
-                  All steps completed - you&apos;re finished
-                </Typography>
-                {/* <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
-                    Reset
-                  </Button> */}
-              </Paper>
-            )}
             {/* </Box> */}
           </Box>
         </Card>
