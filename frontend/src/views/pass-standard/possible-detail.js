a5b348dd7e1d16e7f7b908abe12815f9f5fcb18f
shin-6-0 <syk6715@naver.com>
2024-01-11 15:53:23 +0900

    [style] 가통popper 그림자 설정 및 모서리 둥글게+ code부분 클릭방지 적용

--- frontend/src/views/pass-standard/possible-detail.js
+++ frontend/src/views/pass-standard/possible-detail.js
@@ -97,9 +97,6 @@ const checking = ()=>{
         Notify.failure("이미 존재하는 코드 조합입니다.");
         break;
     }
-    //부분 새로고침
-    
-
   };
 
   const processColumn = [
@@ -107,7 +104,11 @@ const checking = ()=>{
     { field: "공정",headerName:processName, width:160, type:'text',alignItems:'left',headerAlign: "left"},
   ]
   return (
-    <>
+    <div style={{
+      borderRadius: "6px",
+      boxShadow: "0px 2px 10px 0px rgba(58, 53, 65, 0.1)",
+      overflow: "hidden", // Optional: 컴포넌트 내용이 박스 쉐도우를 벗어나는 경우 숨김
+    }}>
       <TableContainer>
         <Table aria-label="spanning table">
           <TableHead>
@@ -139,7 +140,8 @@ const checking = ()=>{
                   color: "#05507d",
                   background:"#F5F9FF",
                   fontSize:"15px",
-                  padding:"10px"
+                  padding:"3%",
+                  paddingLeft:"25%"
                 }}
               >
                 Code
@@ -151,7 +153,8 @@ const checking = ()=>{
                   color: "#05507d",
                   background:"#F5F9FF",
                   fontSize:"15px",
-                  padding:"10px"
+                  padding:"3%",
+                  paddingLeft:"17%"
                 }}
               >
                 {processName}
@@ -165,15 +168,15 @@ const checking = ()=>{
           processFactoryList.map((e)=>{
             return(
               <TableRow key={e.id}>
-                <TableCell style={{width:"30%"}}>
-                  {e.processCd}
-                </TableCell>
-                <TableCell padding="checkbox" style={{width:"20%"}}>
+                  <TableCell padding="checkbox" style={{width:"20%"}}>
                   <Checkbox
                     checked={checkedItemList.includes(e.firmPsFacTp)}
                     onChange={(event) => handleCheckboxChange(event, e.firmPsFacTp,e.cdExpl)}                                                                                    
                     style={{margin:0,padding:0}}
                   />
+                </TableCell>
+                <TableCell style={{width:"30%"}}>
+                  {e.processCd}
                 </TableCell>
                 <TableCell
                   align="center" 
@@ -200,15 +203,15 @@ const checking = ()=>{
               </Button>
               </TableCell>
               <TableCell style={{padding:0,margin:0}}>
-              <Button size="small" type="submit" variant="contained" style={{ backgroundColor: "#BE2E22",color:"white" }} onClick={()=>{openFun(false)}}>
-                X
+              <Button size="small" type="submit" variant="contained" style={{ backgroundColor: "#BE2E22",color:"white",margin:"10px" }} onClick={()=>{openFun(false)}}>
+                닫기
               </Button>
               </TableCell>
             </TableRow>
           </TableHead>
         </Table>
       </TableContainer>
-    </>
+    </div>
   );
 }
 
