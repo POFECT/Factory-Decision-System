d4005d35b713a646cee818b96f9800f37c23c655
백재원 <bjw1622@gmail.com>
2024-01-17 14:12:19 +0900

    [대시보드] top 대시보드 수정

--- frontend/src/views/dashboard/PassProcessDash.js
+++ frontend/src/views/dashboard/PassProcessDash.js
@@ -1,51 +1,92 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Link, Typography } from "@mui/material";
 import Card from "@mui/material/Card";
 import { useEffect, useState } from "react";
 import PassStandardApi from "src/pages/api/pofect/ProcessStandardApi";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpLong } from "@fortawesome/free-solid-svg-icons";
+import { faDownLong } from "@fortawesome/free-solid-svg-icons";
+import { faMinus } from "@fortawesome/free-solid-svg-icons";
+
 const PassProcessDash = () => {
   const [passStandard, setPassStandard] = useState([]);
-
+  const count = 50;
   useEffect(() => {
     PassStandardApi.getList((data) => {
       setPassStandard(data.response);
-      console.log(data.response);
     });
   }, []);
   return (
-    <Card elevation={3} style={{ padding: "15px" }}>
+    <Card
+      elevation={3}
+      style={{
+        padding: "15px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
       <div>경유 공정 기준</div>
       <div
         style={{
           display: "flex",
           justifyContent: "space-around",
           marginTop: "20px",
+          alignItems: "center",
         }}
       >
-        <div>
+        <div style={{ display: "flex" }}>
           <Typography variant="h4" sx={{ mr: 2 }} style={{ color: "#212121" }}>
-            52개
+            {passStandard.length}개
           </Typography>
-          <div style={{ color: "#4dd6d3" }}>+2</div>
+          {passStandard.length - count > 0 ? (
+            <div style={{ color: "red" }}>+{passStandard.length - count}</div>
+          ) : null}
+          {passStandard.length - count < 0 ? (
+            <div style={{ color: "blue" }}>{passStandard.length - count}</div>
+          ) : null}
         </div>
-        <ArrowUpwardIcon
-          style={{
-            fontWeight: "bold",
-            width: "60px",
-            height: "50px",
-            color: "#4dd6d3",
-          }}
-        />
+        {passStandard.length - count > 0 ? (
+          <FontAwesomeIcon icon={faUpLong} size="2x" style={{ color: "red" }} />
+        ) : null}
+        {passStandard.length - count < 0 ? (
+          <FontAwesomeIcon
+            icon={faDownLong}
+            size="2x"
+            style={{ color: "blue" }}
+          />
+        ) : null}
+        {passStandard.length - count === 0 ? (
+          <FontAwesomeIcon
+            icon={faMinus}
+            size="2x"
+            style={{ color: "black" }}
+          />
+        ) : null}
       </div>
-      <Button
-        size="small"
-        type="submit"
-        variant="contained"
-        style={{ backgroundColor: "#E29E21" }}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "90%",
+          justifyContent: "center",
+        }}
       >
-        경유 공정
-      </Button>
+        <Link href="/pass-standard/" style={{ width: "100%" }}>
+          <Button
+            size="small"
+            type="submit"
+            variant="contained"
+            style={{
+              backgroundColor: "rgb(10, 83, 128)",
+              whiteSpace: "nowrap",
+              fontSize: "15px",
+              width: "100%",
+            }}
+          >
+            경유 공정 기준
+          </Button>
+        </Link>
+      </div>
     </Card>
   );
 };
